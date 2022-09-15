a0026ca0c45989b166f3480ccb3bf0d321b21c8e
HanningtoneM <hanningtonepompyy@gmail.com>
2022-09-15 14:35:45 +0300

    Fix issue with previous database

--- app/models/comment.rb
+++
@@ -1,4 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :author_id
-  belongs_to :post_id
-end
--- app/models/like.rb
+++
@@ -1,4 +0,0 @@
-class Like < ApplicationRecord
-  belongs_to :author_id
-  belongs_to :post_id
-end
