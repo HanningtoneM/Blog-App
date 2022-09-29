75f171f4ec3a0681ff6693fac98ab1f42e751238
HanningtoneM <hanningtonepompyy@gmail.com>
2022-09-29 15:37:58 +0300

    Migrate to fix issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,1 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root to: 'users#index'
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create, :destroy] do
-      resources :comments, only: [:index, :create, :destroy]
-      resources :likes, only: [:create, :destroy]
-    end
-  end
 
-end
